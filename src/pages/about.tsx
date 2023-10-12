7da52c34b02dcdf29df0a2211a167832214ef497
jacktrusler <jtrus93@gmail.com>
2023-10-12 10:58:59 -0400

    about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,53 @@
+import LayoutAndNavbar from '@/components/Pages/LayoutAndNavbar';
+import Image from 'next/image';
+
+export default function about() {
+  return (
+    <LayoutAndNavbar>
+      <div className="flex flex-col items-center w-full pl-4">
+        <div className="flex w-full flex-col">
+          <div className="flex justify-center gap-20">
+            <div className="flex">
+              <span className="flex pt-8 items-center justify-center font-semibold tracking-[4px] text-secondary-700">
+                <Image
+                  src="/about-pandasia.png"
+                  alt="Pandasia Logo"
+                  width={250}
+                  height={250}
+                  priority
+                />
+              </span>
+            </div>
+            <div className="flex flex-col max-w-[600px]">
+              <span className="flex pt-8 font-semibold text-2xl tracking-[4px] text-primary-600">
+                PANDASIA
+              </span>
+              <span className="flex pt-2 text-lg">
+                Pandasia (Πανδαισία) is Greek, meaning "banquet for everyone". This echos the design
+                philosophy of the site, we wanted a place for people to come from all over the
+                Avalanche ecosystem and have a place to provide rewards to the people most
+                integrated in Avalanche through airdrops.
+              </span>
+              <span className="flex pt-2 text-lg">
+                More technically, we wanted to have a verifiable way for validators of subnets to be
+                rewarded for their commitment to the space. In order to provide this we came up with
+                a way to tie a C-chain address to a P-chain address that is attached to a validator
+                node. In doing so, we can provide airdrops directly to your C-chain wallet address.
+                Welcome to the symposium for Avalanche's most dedicated users.
+              </span>
+              <span className="flex pt-8 font-semibold text-2xl tracking-[4px] text-primary-600">
+                WHO WE ARE
+              </span>
+              <span className="flex pt-2 text-lg">
+                We are the GoGoPool team. Our goal is to provide a permissionless staking protocol
+                designed to create widespread adoption for Subnets. This website is an extension of
+                that desire. You can check out our main site or you can visit our socials by using
+                the links on the Navbar at the top of the page.
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </LayoutAndNavbar>
+  );
+}
