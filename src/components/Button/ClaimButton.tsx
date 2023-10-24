5d8c53015e5793d18031079e04732dc6bf704307
jacktrusler <jtrus93@gmail.com>
2023-10-24 16:26:30 -0400

    claim button console error fix

--- src/components/Button/ClaimButton.tsx
+++ src/components/Button/ClaimButton.tsx
@@ -24,10 +24,20 @@ export default function ClaimButton({
 }: Props) {
   const { pChainAddr } = useC2PAuth();
   const [proof, setProof] = useState<HexString[]>([]);
+  const { config, error } = useClaimAirdrop(contractId, proof);
+  const { write: claim } = useContractWrite({ ...config });
 
   useEffect(() => {
     fetchProof();
   }, []);
+
+  useEffect(() => {
+    const regex = /Error: (.*)/;
+    const errorMaybe = error?.message.match(regex);
+    if (errorMaybe) {
+      setError(errorMaybe[1]);
+    }
+  }, [setError, error]);
 
   async function fetchProof() {
     const { data: proofy } = await getProof(root, pChainAddr || '', '');
@@ -47,9 +57,6 @@ export default function ClaimButton({
     }
   }
 
-  const { config, error } = useClaimAirdrop(contractId, proof);
-  const { write: claim } = useContractWrite({ ...config });
-
   function handleClaim() {
     if (claim) {
       claim();
@@ -60,17 +67,11 @@ export default function ClaimButton({
     recordClick();
   }
 
-  const regex = /Error: (.*)/;
-  const errorMaybe = error?.message.match(regex);
-  if (errorMaybe) {
-    setError(errorMaybe[1]);
-  }
-
   return (
     <button
       onClick={() => handleClaim()}
       className="border disabled:cursor-not-allowed disabled:border-secondary-600 disabled:text-secondary-600 cursor-pointer border-white px-4 py-2 text-xs transition-colors hover:border-primary-500 hover:text-primary-500 font-semibold tracking-widest"
-      disabled={errorMaybe ? true : false}
+      disabled={error ? true : false}
     >
       CLAIM
     </button>
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -14,19 +14,17 @@ export default function Logo({ erc20Address, logo, size }: Props) {
     `https://raw.githubusercontent.com/traderjoe-xyz/joe-tokenlists/main/logos/${erc20Address}/logo.png`,
   );
 
-  const randomish = Math.floor(Math.random() * 100 + 100);
-
   const reggy = /http/;
   const hasHttp = reggy.test(logo);
 
   async function logoFetch() {
     if (hasHttp) {
       try {
-        const res = await axios.get(logo);
+        await axios.get(logo);
         setLogoUrl(logo);
       } catch {
         try {
-          const res = await axios.get(
+          await axios.get(
             `https://raw.githubusercontent.com/traderjoe-xyz/joe-tokenlists/main/logos/${erc20Address}/logo.png`,
           );
           setLogoUrl(
@@ -38,7 +36,7 @@ export default function Logo({ erc20Address, logo, size }: Props) {
       }
     } else {
       try {
-        const res = await axios.get(
+        await axios.get(
           `https://raw.githubusercontent.com/traderjoe-xyz/joe-tokenlists/main/logos/${erc20Address}/logo.png`,
         );
         setLogoUrl(
