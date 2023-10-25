ab9c6e655c475d8279f1e10ab93aa36c0f8e45c8
jacktrusler <jtrus93@gmail.com>
2023-10-25 12:21:49 -0400

    changed css for page transitions

--- src/components/Pages/PageTransitions.tsx
+++ src/components/Pages/PageTransitions.tsx
@@ -2,37 +2,15 @@ import { motion } from 'framer-motion';
 import { ReactNode } from 'react';
 
 export function FadeTransition({ children }: { children: ReactNode }) {
-  return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{
-        type: 'color',
-        duration: 1,
-      }}
-    >
-      {children}
-    </motion.div>
-  );
+  return <div className="fade-in">{children}</div>;
 }
 
 export function AccordianTransition({ children }: { children: ReactNode }) {
   return (
-    <motion.div>
+    <div>
       {children}
-      <motion.div
-        className="slide-left"
-        initial={{ x: 0, opacity: 1 }}
-        animate={{ x: -1000, opacity: 0 }}
-        transition={{ duration: 3, ease: [0.22, 1, 0.36, 1] }}
-      ></motion.div>
-      <motion.div
-        className="slide-right"
-        initial={{ x: 0, opacity: 1 }}
-        animate={{ x: 1000, opacity: 0 }}
-        transition={{ duration: 3, ease: [0.22, 1, 0.36, 1] }}
-      ></motion.div>
-    </motion.div>
+      <div className="slide-left"></div>
+      <div className="slide-right"></div>
+    </div>
   );
 }
