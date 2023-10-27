5a9e150a703c585fafff4a3132b5f4624261cc0a
jacktrusler <jtrus93@gmail.com>
2023-10-27 14:17:49 -0400

    secret tunnel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,13 +9,19 @@ import Lottie from 'lottie-react';
 import pandasiaBg from '@/styles/lottie/pandasia-background.json';
 import { getAccount } from 'wagmi/actions';
 import { useAccount } from 'wagmi';
+import { useRouter } from 'next/router';
 
 export default function Home() {
   const { address } = useAccount();
   const { isConnected } = getAccount();
-  const [isClient, setIsClient] = useState(false);
   const { pChainAddr } = useC2PAuth();
+  const router = useRouter();
 
+  const [s1, setS1] = useState(false);
+  const [s2, setS2] = useState(false);
+  const [s3, setS3] = useState(false);
+  const [s4, setS4] = useState(false);
+  const [isClient, setIsClient] = useState(false);
   useEffect(() => {
     setIsClient(true);
   }, []);
@@ -47,6 +53,21 @@ export default function Home() {
     );
   };
 
+  if (s1 && s2 && s3 && s4) {
+    return (
+      <main className="hs-bg relative flex h-screen flex-col bg-secondary-800 items-center justify-center">
+        <iframe
+          width="1000"
+          height="630"
+          src="https://www.youtube.com/embed/7o4EI_-5reA?si=vjMuVkyBRT-Co-8l"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen={true}
+        ></iframe>
+      </main>
+    );
+  }
+
   return (
     <FadeTransition>
       <main className={`welcome-bg relative flex min-h-screen flex-col justify-center`}>
@@ -59,7 +80,23 @@ export default function Home() {
               alt="pandasia logo"
               src={'/favicon.svg'}
             />
-            <Lottie animationData={pandasiaBg} loop={false} style={{ height: '100vh' }} />
+            <div className="relative">
+              <div onClick={() => setS1(true)} className="left-28 absolute z-10 w-14 h-12"></div>
+              <div onClick={() => setS2(true)} className="right-28 absolute z-10 w-14 h-12"></div>
+              <div
+                onClick={() => setS3(true)}
+                className="left-28 bottom-0 z-10 absolute w-14 h-12"
+              ></div>
+              <div
+                onClick={() => setS4(true)}
+                className="right-28 bottom-0 z-10 absolute w-14 h-12"
+              ></div>
+              <Lottie
+                animationData={pandasiaBg}
+                loop={false}
+                style={{ height: '100vh', position: 'relative' }}
+              />
+            </div>
             <div className="absolute top-1/2 z-10 mt-[-12px] border-4 border-primary-600 bg-secondary-900 p-1">
               {displayButton()}
             </div>
