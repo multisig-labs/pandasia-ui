656faa066518056d0fa24d15de8c95777f692e64
Kyle <kyletrusler@gmail.com>
2025-03-18 09:23:30 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,19 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com',
-        pathname: '/traderjoe-xyz/**',
-        port: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-      },
-    ],
+    unoptimized: true,
   },
 };
 
