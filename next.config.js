aa94a5fbcb8ff8a79831e9aa057f7e6af6e7997f
Kyle <kyletrusler@gmail.com>
2025-03-26 08:09:43 -0500

    Whitelist only traderjoe-xyz content in next.config.js (#36)
    * Whitelist only traderjoe-xyz content in next.config.js
    
    * Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,18 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com',
-        port: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-      },
-    ],
+    unoptimized: true,
   },
 };
 
